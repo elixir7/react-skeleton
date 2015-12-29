bd4deb0614d84d166182bddf27920a9768a14542
Isak <aslundisak@gmail.com>
2015-12-30 00:31:27 +0100

    Updates

---
+++ src/Routes.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+
+var CreateHistory = require('history/lib/createHashHistory');
+
+var TodoApp3 = require('./components/TodoApp3.jsx');
+
+//Removes the haskey from the url and shows the page name in text
+var History = new CreateHistory({
+  queryKey: false
+});
+
+var Routes = (
+  <Router history={History}>
+      <Route path="/" component={TodoApp3} >
+      </Route>
+  </Router>
+);
+
+module.exports = Routes;
